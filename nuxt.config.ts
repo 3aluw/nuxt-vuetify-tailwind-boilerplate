a5a265503735d04e9568485f02f6e6e675f701f1
Abdou <2000abdallah2014@gmail.com>
2023-04-03 11:04:48 +0100

    added vuetigy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-    modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt','@nuxtjs/tailwindcss'],
+  css: ['vuetify/lib/styles/main.sass','@mdi/font/css/materialdesignicons.min.css',],
+    modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt','@nuxtjs/tailwindcss',],
     pinia: {
       autoImports: [
         // automatically imports `defineStore`
@@ -9,4 +9,12 @@ export default defineNuxtConfig({
         ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
       ],
     },
+      build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
