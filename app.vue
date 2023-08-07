4fcdf5e902ee7afe278f94323dd79917b4ceaf0b
3aluw <82264545+3aluw@users.noreply.github.com>
2023-08-07 06:56:58 +0000

    Update 3 files

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <v-responsive
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-text-field
+      label="First name"
+      hide-details="auto"
+    ></v-text-field>
+  </v-responsive>
 </template>
