9332536ed6585b55b480e3a1f9989bd3925c0037
Abdou <82264545+3aluw@users.noreply.github.com>
2024-08-01 09:24:28 +0000

    updated packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['vuetify/lib/styles/main.sass','@mdi/font/css/materialdesignicons.min.css',],
-    modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt','@nuxtjs/tailwindcss','@vueuse/nuxt',],
-    pinia: {
-      autoImports: [
-        // automatically imports `defineStore`
-        'defineStore', // import { defineStore } from 'pinia'
-        ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
-      ],
-    },
-      build: {
-    transpile: ['vuetify'],
+  modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt','@nuxtjs/tailwindcss','@vueuse/nuxt',],
+
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      'defineStore', // import { defineStore } from 'pinia'
+      ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
   },
+
+  build: {
+transpile: ['vuetify'],
+},
+
   vite: {
     define: {
       'process.env.DEBUG': false,
     },
   },
+
+  compatibilityDate: '2024-08-01',
 })
